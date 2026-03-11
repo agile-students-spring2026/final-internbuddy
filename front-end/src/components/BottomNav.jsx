70df5ef2d8e36ce4fd5594ed536d192ba5af7694
alissawu <aw5571@nyu.edu>
2026-03-11 01:42:33 -0400

    wired bottomnav home page to events

--- front-end/src/components/BottomNav.jsx
+++ front-end/src/components/BottomNav.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom'
 import './BottomNav.css'
 
 function BottomNav() {
   return (
     <div className="bottom-nav">
-      {/* TODO: replace buttons with <Link> from react-router-dom once other pages exist */}
-      <button className="nav-btn">Home</button>
+      <Link className="nav-btn" to="/events">Home</Link>
+      {/* TODO: wire up once search + profile pages exist */}
       <button className="nav-btn">Search</button>
       <button className="nav-btn">Profile</button>
     </div>
