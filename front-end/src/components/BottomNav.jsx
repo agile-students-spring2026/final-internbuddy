6540e2b6c63fd097942443260c762e00cc287ac7
alissawu <aw5571@nyu.edu>
2026-03-11 01:40:14 -0400

    TODO abt react router

--- front-end/src/components/BottomNav.jsx
+++ front-end/src/components/BottomNav.jsx
@@ -3,6 +3,7 @@ import './BottomNav.css'
 function BottomNav() {
   return (
     <div className="bottom-nav">
+      {/* TODO: replace buttons with <Link> from react-router-dom once other pages exist */}
       <button className="nav-btn">Home</button>
       <button className="nav-btn">Search</button>
       <button className="nav-btn">Profile</button>
