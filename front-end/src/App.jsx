10547e12f3b9eed2dc8a0e48f80b902874e966b8
CharlieLi111 <lisongyang2023@163.com>
2026-03-18 13:23:03 -0400

    Add join meetup functionality, location field, button styling, and connect create events to home feed using React Context

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { EventsProvider } from './context/EventsContext'
 import LoginPage from './pages/LoginPage'
 import ForgotPasswordPage from './pages/ForgotPasswordPage'
 import EventsPage from './pages/EventsPage'
@@ -8,21 +9,21 @@ import CreateEventsPage from './pages/CreateEventsPage'
 import BottomNav from './components/BottomNav'
 import './App.css'
 
-// removed the default vite template stuff
-// TODO: show login page if not logged in, events page if logged in. rn just shows login page.
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/events" element={<EventsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/your-events" element={<YourEventsPage />} />
-        <Route path="/create-events" element={<CreateEventsPage />} />
-      </Routes>
-      <BottomNav />
-    </Router>
+    <EventsProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/your-events" element={<YourEventsPage />} />
+          <Route path="/create-events" element={<CreateEventsPage />} />
+        </Routes>
+        <BottomNav />
+      </Router>
+    </EventsProvider>
   )
 }
 
