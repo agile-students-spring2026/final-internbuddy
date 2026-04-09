e85278d535a48f5d13356987b9a6c77246d9ce41
alissawu <aw5571@nyu.edu>
2026-04-08 20:42:57 -0400

    remove ping placeholder route

--- back-end/src/routes/connectionsRoutes.js
+++ back-end/src/routes/connectionsRoutes.js
@@ -10,11 +10,6 @@ const {
 
 const router = express.Router();
 
-// placeholder route to make sure wiring works
-router.get('/ping', (req, res) => {
-  res.json({ message: 'connections route is alive' });
-});
-
 router.post('/request', sendRequest);
 router.get('/:userId/pending', getPending);
 router.get('/:userId', getAccepted);
