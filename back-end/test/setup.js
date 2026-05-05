3d048a75f52256a2ed310055498895aa2cfccf63
alissawu <aw5571@nyu.edu>
2026-05-05 17:18:37 -0400

    test: stop in-memory mongod in afterAll so CI doesn't hang after tests pass

--- back-end/test/setup.js
+++ back-end/test/setup.js
@@ -5,7 +5,7 @@ require('dotenv').config({ path: '.env.test', override: true });
 process.env.ALLOW_IN_MEMORY_DB = 'true';
 
 const mongoose = require('mongoose');
-const { connectDB } = require('../src/db');
+const { connectDB, disconnectDB } = require('../src/db');
 
 if (!process.env.JWT_SECRET) {
   process.env.JWT_SECRET = 'internbuddy-test-secret';
@@ -18,8 +18,8 @@ exports.mochaHooks = {
     }
   },
   async afterAll() {
-    if (mongoose.connection.readyState !== 0) {
-      await mongoose.disconnect();
-    }
+    // disconnectDB also stops the in-memory mongod child process, otherwise
+    // node won't exit and CI hangs after tests finish.
+    await disconnectDB();
   },
 };
